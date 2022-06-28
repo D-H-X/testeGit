d4c6fd56946d200a57693d8f2f4ec51cab0fc1d8
supor <supor@28.0.125.153>
2022-06-28 00:38:58 -0300

    Teste GitHub

---
+++ testeGit/src/main/java/br/com/dhx/TesteGitApplication.java
@@ -0,0 +1,13 @@
+package br.com.dhx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesteGitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TesteGitApplication.class, args);
+	}
+
+}
---
+++ testeGit/src/test/java/br/com/dhx/TesteGitApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.dhx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TesteGitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
